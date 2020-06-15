74e25acb49414d88e066bccebc82136e7bc91ed2
miki123k <mebs1dpanntly8uy@gmail.com>
2020-06-15 11:46:23 +0900

    このコードで期待する動作が出力することを確認

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,9 +2,21 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+    for($j = 0; $j <$count; $j++){
+        //$iより$jの値が大きければ
+        if ($array[$i] < $array[$j]){
+            //$tmpに$iを一旦代入
+            $tmp = $array[$i];
+            //$iに$jを代入して入れ替える
+            $array[$i] = $array[$j];
+            //$jに$tmpを代入
+            $array[$j] = $tmp; 
+        }
+    }
 }
+//参考にした動画はこちらです。https://www.youtube.com/watch?v=WY-_iKLNhA0
 // 修正はここまで
 
 echo "<pre>";
