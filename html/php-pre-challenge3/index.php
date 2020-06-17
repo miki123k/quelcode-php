1268fc357490dd52307b59f495af7cd3c6d73767
miki123k <mebs1dpanntly8uy@gmail.com>
2020-06-18 07:26:45 +0900

    GETパラメータの値チェック

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,6 +1,12 @@
 <?php
 $limit = $_GET['target'];
 
+//$limitが1以上の整数かチェック
+if (!preg_match("/^[\d]+$/", $limit) || $limit <= 0 || preg_match("/^[0]/", $limit)) { //数字以外をチェック, 0以下をチェック, 先頭が０をチェック
+    http_response_code(400); //1以上の整数でない場合はHTTPレスポンスステータスコード400で返す
+    exit();
+}
+
 
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
