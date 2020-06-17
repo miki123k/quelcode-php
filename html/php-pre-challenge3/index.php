389db792b9459660308896f14d3d3ccdbd22926a
miki123k <mebs1dpanntly8uy@gmail.com>
2020-06-18 07:26:14 +0900

    データベースへの接続

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,6 +1,15 @@
 <?php
 $limit = $_GET['target'];
 
+
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
+
+//データベースに接続
+try {
+    $db = new PDO($dsn, $dbuser, $dbpassword); //データベースオブジェクトの作成
+} catch (PDOException $e) {
+    http_response_code(500); //データベースに接続できない際はHTTPレスポンスステータスコード500で返す
+    exit();
+}
