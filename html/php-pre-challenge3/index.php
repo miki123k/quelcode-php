e3093b666b355f21e9ba368dbbfff23b90ab4ace
miki123k <mebs1dpanntly8uy@gmail.com>
2020-06-22 11:32:34 +0900

    受け取った文字列を整数に変換

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -6,6 +6,7 @@ if (!preg_match("/^[\d]+$/", $limit) || $limit <= 0 || preg_match("/^[0]/", $lim
     http_response_code(400); //1以上の整数でない場合はHTTPレスポンスステータスコード400で返す
     exit();
 }
+$limit = (int) $limit; //受け取ったものはstring(文字列)型なのでint(整数)型に変換する
 
 
 $dsn = 'mysql:dbname=test;host=mysql';
@@ -19,3 +20,11 @@ try {
     http_response_code(500); //データベースに接続できない際はHTTPレスポンスステータスコード500で返す
     exit();
 }
+
+//データベースから値をとってくる
+
+//全ての数字の組み合わせをつくる（コピペでOK）2×８−１＝２５６の組み合わせになる
+
+//作られた組み合わせから合計と一致するものを出力
+
+//それをJSON形式で出力
