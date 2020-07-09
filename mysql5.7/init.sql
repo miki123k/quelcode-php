658539240e33486bbc43d0b48242341dbee4bd66
miki123k <mebs1dpanntly8uy@gmail.com>
2020-07-09 13:20:08 +0900

    全角スペース削除

--- mysql5.7/init.sql
+++ mysql5.7/init.sql
@@ -14,8 +14,8 @@ CREATE TABLE `posts`(
       `message` text NOT NULL,
       `member_id` int(11) NOT NULL,
       `reply_post_id` int(11) NOT NULL,
-      `retweet_post_id`　int(11) NOT NULL,
-      `retweet_member_id`　int(11) NOT NULL,
+      `retweet_post_id` int(11) NOT NULL,
+      `retweet_member_id` int(11) NOT NULL,
       `created` datetime NOT NULL,
       `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON　UPDATE CURRENT_TIMESTAMP,
       PRIMARY KEY(`id`)
@@ -32,4 +32,4 @@ CREATE TABLE `prechallenge3`(
       `value` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `prechallenge3` (`value`)　VALUES　(1),(17),(3),(13),(11),(7),(19),(5);
+INSERT INTO `prechallenge3` (`value`) VALUES (1),(17),(3),(13),(11),(7),(19),(5);
